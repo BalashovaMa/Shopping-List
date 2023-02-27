fdd3af5cc7eb55400ea6458d26f7d0f4cb54a38d
BalashovaMa <balashova.m.nd@gmail.com>
2023-02-27 10:05:28 +0200

    feat: add removing and adding elements

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-$('li').click(function () {
+$('ul').on('click', 'li', function () {
     $(this).toggleClass("done");
 });
+$('ul').on('click', 'span', function (event) {
+    event.stopPropagation();
+    $(this).parent().fadeOut(function () {
+        $(this).remove();
+    });
+});
+$('input').keypress(function (event) {
+    if (event.which === 13) {
+        
+        let itemText = $(this).val();
+        $(this).val('');
+        $('ul').append('<li><span>X </span>' + itemText + '</li>');
+    }
+})
